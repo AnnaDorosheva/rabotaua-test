47534947cebd5544b7fab88f62473ca2e8588bad
AnnaDorosheva <annador@ukr.net>
2022-01-21 16:40:31 +0200

    Updates

---
+++ precache-manifest.0e410340e8652664f38309e2fc7fa729.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94876f2ed2e86ce6ff20ea5a01f4b3",
+    "url": "/rabotaua-test/index.html"
+  },
+  {
+    "revision": "9597cc0e0e257218cd75",
+    "url": "/rabotaua-test/static/css/main.38caa38b.chunk.css"
+  },
+  {
+    "revision": "413ce01d553d26a40ea5",
+    "url": "/rabotaua-test/static/js/0.eb9fd29e.chunk.js"
+  },
+  {
+    "revision": "3a7c80b717f1dd0c0287",
+    "url": "/rabotaua-test/static/js/5.a68ee6ed.chunk.js"
+  },
+  {
+    "revision": "7c5ed637099362b99b5e",
+    "url": "/rabotaua-test/static/js/6.fefb2db8.chunk.js"
+  },
+  {
+    "revision": "9597cc0e0e257218cd75",
+    "url": "/rabotaua-test/static/js/main.a36355aa.chunk.js"
+  },
+  {
+    "revision": "5a59953a96a82fe089d6",
+    "url": "/rabotaua-test/static/js/runtime~main.e0ca8e74.js"
+  },
+  {
+    "revision": "eb34bc7ed8ce1185b249",
+    "url": "/rabotaua-test/static/js/ucare-panel-chunk.0445929f.chunk.js"
+  },
+  {
+    "revision": "e2d1f0f5e84fdd89f406",
+    "url": "/rabotaua-test/static/js/ucare-widget-chunk.9b6fccc4.chunk.js"
+  }
+]);
